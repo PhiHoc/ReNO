e074d58512bd2dc50e023f2601906845c80eaef9
krail <krail@creqavn.net>
2025-06-08 17:20:57 +0700

    fear: add script to run and add 20 prompt for each class in datasets

---
+++ run.bat
@@ -0,0 +1,5 @@
+py main.py ^
+    --task bear_dataset ^
+    --model sdxl-turbo ^
+    --save_dir ./outputs_bears ^
+    --num_images_per_prompt 1
---
+++ run.sh
@@ -0,0 +1,5 @@
+python main.py \
+    --task bear_dataset \
+    --model sdxl-turbo \
+    --save_dir ./outputs_bears \
+    --num_images_per_prompt 1
